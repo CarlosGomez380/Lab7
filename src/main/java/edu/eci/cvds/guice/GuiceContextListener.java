407c8144a0a83a139b5b6c2f2d2aafcd3036e21c
CarlosGomez380 <gemec98@gmail.com>
2019-10-15 22:22:00 -0500

    last commit

---
+++ src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
@@ -0,0 +1,45 @@
+package edu.eci.cvds.guice;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.mybatis.guice.XMLMyBatisModule;
+import org.mybatis.guice.datasource.helper.JdbcHelper;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import edu.eci.cvds.sampleprj.dao.ClienteDAO;
+import edu.eci.cvds.sampleprj.dao.ItemDAO;
+import edu.eci.cvds.sampleprj.dao.TipoItemDAO;
+import edu.eci.cvds.sampleprj.dao.mybatis.MyBATISClienteDAO;
+import edu.eci.cvds.sampleprj.dao.mybatis.MyBATISItemDAO;
+import edu.eci.cvds.sampleprj.dao.mybatis.MyBATISTipoItemDAO;
+import edu.eci.cvds.samples.services.ServiciosAlquiler;
+import edu.eci.cvds.samples.services.impl.ServiciosAlquilerImpl;
+
+public class GuiceContextListener implements ServletContextListener {
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.removeAttribute(Injector.class.getName());
+    }
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        Injector injector = Guice.createInjector(new XMLMyBatisModule() {
+            @Override
+            protected void initialize() {
+                install(JdbcHelper.MySQL);
+                setEnvironmentId("development");
+                setClassPathResource("mybatis-config.xml");
+
+                // TODO Add service class associated to Stub implementation
+                bind(ServiciosAlquiler.class).to(ServiciosAlquilerImpl.class);
+                bind(ItemDAO.class).to(MyBATISItemDAO.class);
+                bind(ClienteDAO.class).to(MyBATISClienteDAO.class);
+                bind(TipoItemDAO.class).to(MyBATISTipoItemDAO.class);
+                
+            }
+        });
+
+        servletContextEvent.getServletContext().setAttribute(Injector.class.getName(), injector);
+    }
+}
